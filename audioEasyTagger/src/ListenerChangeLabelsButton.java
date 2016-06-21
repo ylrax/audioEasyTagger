0cf2331fcd8eb8c5cc5189356054602f3881551c
HugoJBello <hugo.bello.gu@gmail.com>
2016-06-21 19:06:28 +0200

    minor changes, configuring jlists

--- audioEasyTagger/src/ListenerChangeLabelsButton.java
+++ audioEasyTagger/src/ListenerChangeLabelsButton.java
@@ -6,7 +6,10 @@ import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
+import javax.swing.JList;
 import javax.swing.JTextField;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 import org.jaudiotagger.audio.exceptions.CannotReadException;
 import org.jaudiotagger.audio.exceptions.CannotWriteException;
@@ -14,17 +17,32 @@ import org.jaudiotagger.audio.exceptions.InvalidAudioFrameException;
 import org.jaudiotagger.audio.exceptions.ReadOnlyFileException;
 import org.jaudiotagger.tag.KeyNotFoundException;
 import org.jaudiotagger.tag.TagException;
- 
+
 
 public class ListenerChangeLabelsButton {
 
 
-	public ListenerChangeLabelsButton(JButton btnChangeLabels, final JCheckBox chckbxChangeAlbum, final JTextField txtNewAlbum, final ArrayList<File> listOfFiles, final JCheckBox chckbxChangeArtist,final JTextField txtNewArtist){
+	public ListenerChangeLabelsButton(JButton btnChangeLabels, final JCheckBox chckbxChangeAlbum, final JTextField txtNewAlbum, final ArrayList<File> totalListOfFiles, final JList<File> listOfFilesToTag, final JCheckBox chckbxChangeArtist,final JTextField txtNewArtist){
+			
 		btnChangeLabels.addActionListener(new ActionListener() {
-
+			//First we get the selected files whose tags the user wants to change:
 			public void actionPerformed(ActionEvent e) {
+				final ArrayList<File> totalListOfFilesLocal = totalListOfFiles;
 
-				for (final File fileInList : listOfFiles){
+				//Here we change the selected files only
+				int[] selectedIx = listOfFilesToTag.getSelectedIndices();
+				ArrayList<File> selectedFilesToTag = new ArrayList<File>();
+				if (! listOfFilesToTag.isSelectionEmpty()){
+					for (int i = 0; i < selectedIx.length; i++) {
+						selectedFilesToTag.add(totalListOfFilesLocal.get(selectedIx[i]));	
+					}
+					//jlistListOfFilesToTag.clearSelection(); 
+					//jlistListOfFilesToTag.setSelectedIndex(-1);
+					System.out.println(selectedFilesToTag);
+				}
+				else{ 
+					selectedFilesToTag =  totalListOfFilesLocal;}
+				for (final File fileInList : selectedFilesToTag){
 					TagChanger tagger = new TagChanger(fileInList);
 					if (chckbxChangeAlbum.isSelected()){
 						try {
@@ -48,6 +66,7 @@ public class ListenerChangeLabelsButton {
 							// TODO Auto-generated catch block
 							e1.printStackTrace();
 						}
+ 
 					}
 					if (chckbxChangeArtist.isSelected()){
 						try {
@@ -74,12 +93,14 @@ public class ListenerChangeLabelsButton {
 							// TODO Auto-generated catch block
 							e1.printStackTrace();
 						}
+ 
 					}
 
 
 
 				}
 			}
+
 		});
 
 	}
--- audioEasyTagger/src/audioEasyTagger.java
+++ audioEasyTagger/src/audioEasyTagger.java
@@ -1,23 +1,11 @@
 //
 
 import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
-import javax.swing.JFileChooser;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JScrollPane;
-import java.awt.GridBagLayout;
-import javax.swing.JButton;
-import java.awt.GridBagConstraints;
-import javax.swing.JCheckBox;
-import java.awt.Insets;
-import javax.swing.SwingConstants;
-import javax.swing.JTextField;
-
-
 import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
@@ -27,19 +15,28 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
 import javax.swing.border.MatteBorder;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
-import org.jaudiotagger.audio.AudioFile;
-import org.jaudiotagger.audio.AudioFileIO;
 import org.jaudiotagger.audio.exceptions.CannotReadException;
 import org.jaudiotagger.audio.exceptions.InvalidAudioFrameException;
 import org.jaudiotagger.audio.exceptions.ReadOnlyFileException;
-import org.jaudiotagger.tag.FieldKey;
-import org.jaudiotagger.tag.Tag;
 import org.jaudiotagger.tag.TagException;
+
 
 
 public class audioEasyTagger extends JFrame {
@@ -48,7 +45,7 @@ public class audioEasyTagger extends JFrame {
 	private JPanel contentPane;
 	private final JTextField txtNewArtist;
 	private JTextField txtNewAlbum;
-	private JList listOfFilesToTag = new JList<Object>();
+	private JList jlistListOfFilesToTag = new JList<File>();
 	private JList listArtists = new JList<Object>();
 	private JList listAlbums = new JList<Object>();
 
@@ -82,19 +79,21 @@ public class audioEasyTagger extends JFrame {
 
 		setTitle("mp3 tagger");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 767, 434);
+		setBounds(100, 100, 834, 526);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(new BorderLayout(0, 0));
 
+
+
 		final JPanel panel = new JPanel();
 		contentPane.add(panel, BorderLayout.WEST);
 		GridBagLayout gbl_panel = new GridBagLayout();
 		gbl_panel.columnWidths = new int[]{0, 172, 0};
-		gbl_panel.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_panel.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 		gbl_panel.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
-		gbl_panel.rowWeights = new double[]{0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		panel.setLayout(gbl_panel);
 
 		JButton btnNewButton = new JButton("Choose folder");
@@ -155,6 +154,7 @@ public class audioEasyTagger extends JFrame {
 		panel.add(chckbxChangeArtist, gbc_chckbxChangeArtist);
 
 		txtNewArtist = new JTextField();
+		txtNewArtist.setBackground(Color.LIGHT_GRAY);
 		txtNewArtist.setText("New Artist");
 		GridBagConstraints gbc_txtNewArtist = new GridBagConstraints();
 		gbc_txtNewArtist.insets = new Insets(0, 0, 5, 0);
@@ -172,6 +172,7 @@ public class audioEasyTagger extends JFrame {
 		panel.add(chckbxChangeAlbum, gbc_chckbxChangeAlbum);
 
 		txtNewAlbum = new JTextField();
+		txtNewAlbum.setBackground(Color.LIGHT_GRAY);
 		txtNewAlbum.setText("New Album");
 		GridBagConstraints gbc_txtNewAlbum = new GridBagConstraints();
 		gbc_txtNewAlbum.insets = new Insets(0, 0, 5, 0);
@@ -184,18 +185,27 @@ public class audioEasyTagger extends JFrame {
 		final JButton btnChangeLabels = new JButton("Change labels");
 		btnChangeLabels.setBackground(Color.RED);
 		GridBagConstraints gbc_btnChangeLabels = new GridBagConstraints();
+		gbc_btnChangeLabels.insets = new Insets(0, 0, 5, 0);
 		gbc_btnChangeLabels.gridwidth = 2;
 		gbc_btnChangeLabels.gridx = 0;
 		gbc_btnChangeLabels.gridy = 8;
 		panel.add(btnChangeLabels, gbc_btnChangeLabels);
+
+		final JLabel lblNewLabel_1 = new JLabel("0 tags will be changed");
+		GridBagConstraints gbc_lblNewLabel_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1.gridwidth = 2;
+		gbc_lblNewLabel_1.insets = new Insets(0, 0, 0, 5);
+		gbc_lblNewLabel_1.gridx = 0;
+		gbc_lblNewLabel_1.gridy = 9;
+		panel.add(lblNewLabel_1, gbc_lblNewLabel_1);
 
 		final JScrollPane scrollPane = new JScrollPane();
 		contentPane.add(scrollPane, BorderLayout.CENTER);
 
 		JLabel lblFolder = new JLabel("Files:");
 		scrollPane.setColumnHeaderView(lblFolder);
-		listOfFilesToTag.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
-		scrollPane.setViewportView(listOfFilesToTag);
+		jlistListOfFilesToTag.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
+		scrollPane.setViewportView(jlistListOfFilesToTag);
 
 		/////////////////////////////////////////////////////////////////////////////////////////////////
 		// here we track the activity.
@@ -212,16 +222,21 @@ public class audioEasyTagger extends JFrame {
 				JFileChooser f = new JFileChooser();
 				f.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY); 
 				f.showSaveDialog(null);
+				jlistListOfFilesToTag.removeAll();
 
-
-				// we display the chosen file in the panel
+				// we display the chosen folder in the panel
 
 				try {
 
 					final TagAnalyser analyser = new TagAnalyser(f.getSelectedFile());
-
-					listOfFilesToTag= new JList(analyser.getListOfFilesString().toArray());
-					scrollPane.setViewportView(listOfFilesToTag);
+					final int numberOfFilesToTag = analyser.getListOfFilesString().size();
+					  ArrayList<File> totalListOfFiles = new ArrayList<File>();
+					totalListOfFiles = analyser.getListOfFiles();
+					jlistListOfFilesToTag= new JList(analyser.getListOfFilesString().toArray());
+					int[] selectedIx = jlistListOfFilesToTag.getSelectedIndices();
+					for (int i :selectedIx){System.out.println(", " + i);}
+					scrollPane.setViewportView(jlistListOfFilesToTag);
+					lblNewLabel_1.setText(numberOfFilesToTag + " tags will be changed");
 
 					// here we display the Artists and the Albums in the JList
 
@@ -275,15 +290,40 @@ public class audioEasyTagger extends JFrame {
 
 					listAlbums.addMouseListener(mouseListenerAlbum);
 
-					// here we change the labels using the class TagChanger and the library mp3Magic.
-
-
+					// here we change the labels using the class TagChanger.
 					// this part is active when the user presses the btnChangeLabels
 					@SuppressWarnings("unused")
-					ListenerChangeLabelsButton listener = new ListenerChangeLabelsButton(btnChangeLabels, chckbxChangeAlbum,txtNewAlbum, analyser.getListOfFiles(), chckbxChangeArtist, txtNewArtist);
-					
-					
-					
+
+					/*final ArrayList<File> totalListOfFilesLocal = totalListOfFiles;
+
+					ListSelectionListener listSelectionListener = new ListSelectionListener() {
+						public void valueChanged(ListSelectionEvent listSelectionEvent) {
+							System.out.print("First index: " + listSelectionEvent.getFirstIndex());
+							System.out.print(", Last index: " + listSelectionEvent.getLastIndex());
+
+							int[] selectedIx = jlistListOfFilesToTag.getSelectedIndices();
+							ArrayList<File> selectedFilesToTag = new ArrayList<File>();
+							if (! jlistListOfFilesToTag.isSelectionEmpty()){
+								for (int i = 0; i < selectedIx.length; i++) {
+									selectedFilesToTag.add(totalListOfFilesLocal.get(selectedIx[i]));	
+								}
+								//jlistListOfFilesToTag.clearSelection(); 
+								//jlistListOfFilesToTag.setSelectedIndex(-1);
+								System.out.println(selectedFilesToTag);
+							}
+							else{ 
+								selectedFilesToTag =  totalListOfFilesLocal;}
+							ListenerChangeLabelsButton listener = new ListenerChangeLabelsButton(btnChangeLabels, chckbxChangeAlbum,txtNewAlbum, selectedFilesToTag,jlistListOfFilesToTag, chckbxChangeArtist, txtNewArtist);
+
+						}
+					};
+					jlistListOfFilesToTag.addListSelectionListener(listSelectionListener);*/
+
+					ListenerChangeLabelsButton listener = new ListenerChangeLabelsButton(btnChangeLabels, chckbxChangeAlbum,txtNewAlbum, totalListOfFiles,jlistListOfFilesToTag, chckbxChangeArtist, txtNewArtist);
+
+
+
+
 				} catch (IOException | CannotReadException | TagException | ReadOnlyFileException | InvalidAudioFrameException e2) {
 					// TODO Auto-generated catch block
 					e2.printStackTrace();
